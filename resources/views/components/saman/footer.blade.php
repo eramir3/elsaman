aaed85c24fba7cf78d918088035ce485799a56b5
Esteban Ramirez <estebanramirezcc@gmail.com>
2021-01-08 21:05:33 -0500

    files rearrangement and some blog

---
+++ resources/views/components/saman/footer.blade.php
@@ -0,0 +1,44 @@
+<div class="footer-top"></div>
+<footer class="main-footer">
+    <div class="footer__container">            
+        <nav>
+            <div class="main-footer__image-container">
+                <a href="{{ route('saman.welcome', app()->getLocale()) }}">
+                    <img src="../images/saman/shared/gray-logo.png" alt="">
+                </a>
+            </div>
+            <div class="main-footer__info">
+                <ul class="main-footer__links">
+                    <li class="main-footer__link">
+                        <a href="#">
+                            <img src="{{ asset('images/saman/shared/social/facebook-green.svg') }}" alt="" />
+                        </a>
+                    </li>
+                    <li class="main-footer__link">
+                        <a href="#">
+                            <img src="{{ asset('images/saman/shared/social/instagram-green.svg') }}" alt="" /></i>
+                        </a>
+                    </li>
+                    <li class="main-footer__link">
+                        <a href="https://wa.me/33788225586">
+                            <img src="{{ asset('images/saman/shared/social/whatsapp-green.svg') }}" alt="" /></i>
+                        </a>
+                    </li>
+                    <li class="main-footer__link">
+                        <a href="#">
+                            <img src="{{ asset('images/saman/shared/social/linkedin-green.svg') }}" alt="" /></i>
+                        </a>
+                    </li>
+                </ul>
+                <p>{{ __('footer.discover_colombian_flavors') }}</p>
+            </div>
+        </nav>
+        <div class="main-footer__legal">
+            <p>{{ Str::upper(__('footer.ci_radco')) }}</p>
+            <p>{{ Str::upper(__('footer.reserved_rights')) }}</p>
+            <p>{{ now()->year }}</p>
+        </div>
+        <br>
+        <br>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ContactController;
 
 /*
@@ -14,37 +17,57 @@ use App\Http\Controllers\ContactController;
 |
 */
 
+Auth::routes();
+
 Route::redirect('/', '/en');
 
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
+
+Route::get('post', [PostController::class, 'show'])->name('saman.post');
+
+
+// Route::get('/admin', function () {
+//     return view('admin/index');
+// })->name('admin.index');
+
 Route::group(['prefix' => '/{locale}'], function() {
+
+    
 
     $locale = Request::segment(1);
     $emptyLocale = '';
 
-    if (!in_array($locale, [
-        Config::get('constants.en'), 
-        Config::get('constants.fr'), 
-        Config::get('constants.es'),
-        $emptyLocale
-    ])) {
-        abort(404);
-    }
+    // if (!in_array($locale, [
+    //     Config::get('constants.en'), 
+    //     Config::get('constants.fr'), 
+    //     Config::get('constants.es'),
+    //     $emptyLocale
+    // ])) {
+    //     abort(404);
+    // }
 
     Route::get('/', function () {
-        return view('welcome');
-    })->name('welcome');
+        return view('saman/welcome');
+    })->name('saman.welcome');
     
     Route::get('about', function () {
-        return view('about');
-    })->name('about');
+        return view('saman/about');
+    })->name('saman.about');
     
     Route::get('products', function () {
-        return view('products');
-    })->name('products');
+        return view('saman/products');
+    })->name('saman.products');
     
     Route::get('contact', function () {
-        return view('contact');
-    })->name('contact');
+        return view('saman/contact');
+    })->name('saman.contact');
 
     Route::post('contact', [ContactController::class, 'mail'])->name('contact.mail');
+
+
+    
+
 });
+
