245fc51c7727c34154d1971d25e1ca4c533f352b
vagrant <vagrant@homestead>
2021-01-12 13:59:06 +0000

    Text for forget password

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -9,6 +9,12 @@
 
                 <div class="card-body">
 
+                    <div class="mb-4 text-sm text-gray-600">
+                        {{ __('Forgot your password? No problem. Just let us know your email 
+                            address and we will email you a password
+                            reset link that will allow you to choose a new one.') }}
+                    </div>
+
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
