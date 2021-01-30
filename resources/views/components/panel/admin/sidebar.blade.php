4aa55fb1863da3455d60d208cc833bc51fe098d9
Esteban Ramirez <estebanramirezcc@gmail.com>
2021-01-29 19:17:19 -0500

    Cleaned sidebar

--- resources/views/components/panel/admin/sidebar.blade.php
+++ resources/views/components/panel/admin/sidebar.blade.php
@@ -48,40 +48,8 @@
 
     <!-- Heading -->
     <div class="sidebar-heading">
-        Categories
-    </div>
-
-    <!-- Nav Item - Post Category -->
-    <li class="nav-item {{ Route::is('categories.posts.index') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('categories.posts.index') }}">
-            <i class="fas fa-fw fa-list-alt"></i>
-            <span>Post</span>
-        </a>
-    </li>
-
-    <!-- Nav Item - Product Category -->
-    <li class="nav-item {{ Route::is('categories.products.index') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('categories.products.index') }}">
-            <i class="fas fa-fw fa-list-alt"></i>
-            <span>Product</span>
-        </a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
         Ecommerce
     </div>
-
-    <!-- Nav Item - Coupons -->
-    <li class="nav-item {{ Route::is('coupons.index') ? 'active' : '' }}">
-        <a class="nav-link" href="{{ route('coupons.index') }}">
-            <i class="fas fa-tags"></i>
-            <span>Coupons</span>
-        </a>
-    </li>
 
     <!-- Nav Item - Products -->
     <li class="nav-item {{ Route::is('products.*') ? 'active' : '' }}">
@@ -115,113 +83,33 @@
         </div>
     </li>
 
+    <!-- Nav Item - Categories -->
+    <li class="nav-item {{ Route::is('categories.*') ? 'active' : '' }}">
+        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseCategories"
+            aria-expanded="true" aria-controls="collapseCategories">
+            <i class="fas fa-fw fa-list-alt"></i>
+            <span>Categories</span>
+        </a>
+        <div id="collapseCategories" class="collapse {{ Route::is('categories.*') ? 'show' : '' }}" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
+            <div class="bg-white py-2 collapse-inner rounded">
+                <h6 class="collapse-header">Post Categories</h6>
+                <a class="collapse-item {{ Route::is('categories.posts.index') ? 'active' : '' }}" href="{{route('categories.posts.index')}}">Post</a>
+                <h6 class="collapse-header">Product Categories</h6>
+                <a class="collapse-item {{ Route::is('categories.products.index') ? 'active' : '' }}" href="{{route('categories.products.index')}}">Product</a>
+            </div>
+        </div>
+    </li>
+
+    <!-- Nav Item - Coupons -->
+    <li class="nav-item {{ Route::is('coupons.index') ? 'active' : '' }}">
+        <a class="nav-link" href="{{ route('coupons.index') }}">
+            <i class="fas fa-tags"></i>
+            <span>Coupons</span>
+        </a>
+    </li>
+
      <!-- Divider -->
-     <hr class="sidebar-divider">
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <li class="nav-item">
-        <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseTwo2"
-            aria-expanded="true" aria-controls="collapseTwo">
-            <i class="fas fa-fw fa-cog"></i>
-            <span>Components</span>
-        </a>
-        <div id="collapseTwo2" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Components:</h6>
-                <a class="collapse-item" href="buttons.html">Buttons</a>
-                <a class="collapse-item" href="cards.html">Cards</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Utilities Collapse Menu -->
-    <li class="nav-item">
-    <a class="nav-link collapsed" href="#" data-toggle="collapse" data-target="#collapseUtilities"
-        aria-expanded="true" aria-controls="collapseUtilities">
-        <i class="fas fa-fw fa-wrench"></i>
-        <span>Utilities</span>
-    </a>
-        <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities"
-        data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Custom Utilities:</h6>
-                <a class="collapse-item" href="utilities-color.html">Colors</a>
-                <a class="collapse-item" href="utilities-border.html">Borders</a>
-                <a class="collapse-item" href="utilities-animation.html">Animations</a>
-                <a class="collapse-item" href="utilities-other.html">Other</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider">
-
-    <!-- Heading -->
-    <div class="sidebar-heading">
-        Addons
-    </div>
-
-    <!-- Nav Item - Pages Collapse Menu -->
-    <!-- <li class="nav-item active">
-        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-            aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item active" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li> -->
-
-    <li class="nav-item active">
-        <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
-            aria-controls="collapsePages">
-            <i class="fas fa-fw fa-folder"></i>
-            <span>Pages</span>
-        </a>
-        <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
-            data-parent="#accordionSidebar">
-            <div class="bg-white py-2 collapse-inner rounded">
-                <h6 class="collapse-header">Login Screens:</h6>
-                <a class="collapse-item" href="login.html">Login</a>
-                <a class="collapse-item" href="register.html">Register</a>
-                <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
-                <div class="collapse-divider"></div>
-                <h6 class="collapse-header">Other Pages:</h6>
-                <a class="collapse-item" href="404.html">404 Page</a>
-                <a class="collapse-item active" href="blank.html">Blank Page</a>
-            </div>
-        </div>
-    </li>
-
-    <!-- Nav Item - Charts -->
-    <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-            <i class="fas fa-fw fa-chart-area"></i>
-            <span>Charts</span>
-        </a>
-    </li>
-
-    <!-- Nav Item - Tables -->
-    <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-            <i class="fas fa-fw fa-table"></i>
-            <span>Tables</span>
-        </a>
-    </li>
-
-    <!-- Divider -->
-    <hr class="sidebar-divider d-none d-md-block">
+     <hr class="sidebar-divider">    
 
     <!-- Sidebar Toggler (Sidebar) -->
     <div class="text-center d-none d-md-inline">
